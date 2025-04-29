b6b1716189339535e13e1b33f4f5c8e90b964759
Eduvieira-1 <edu.vieira1@icloud.com>
2025-04-28 23:14:11 -0300

    using useState

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-
+import Nome from './components/Nome'
+import { useState } from 'react';
 
 function App() {
+  const [aluno, setAluno] = useState('Sujeito Programador');
+
+  function handlerChangeName(name) {
+    setAluno(name)
+  }
+
   return (
     <div>
-      Bem Vindo
+      <h1>Bem vindo</h1>
+      <h2>Olá: {aluno}</h2>
+      <Nome aluno="Eduardo" />
+
+      <button onClick={ () => handlerChangeName('Eduardo césar')}>
+          Mudar nome
+      </button>
     </div>
   );
 }
 
 export default App;
+
+
+
